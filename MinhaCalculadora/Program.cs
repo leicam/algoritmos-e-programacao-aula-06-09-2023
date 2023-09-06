d93306fea3c97e3b095583345c47572a75a0a7c9
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2023-09-06 19:57:05 -0300

    feat: implementação de calculadora com bloco try/catch

---
+++ MinhaCalculadora/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MinhaCalculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite o primeiro valor inteiro: ");
+                int valorDigitadoUm;
+                string valorDigitadoString = Console.ReadLine();
+                valorDigitadoUm = int.Parse(valorDigitadoString);
+
+                Console.WriteLine("Digite o segundo valor inteiro: ");
+                int valorDigitadoDois;
+                valorDigitadoString = Console.ReadLine();
+                valorDigitadoDois = int.Parse(valorDigitadoString);
+
+                Console.WriteLine("Resultados");
+                Console.WriteLine("Soma:" + (valorDigitadoUm + valorDigitadoDois));
+                Console.WriteLine("Subtração: " + (valorDigitadoUm - valorDigitadoDois));
+                Console.WriteLine("Divisão: " + (valorDigitadoUm / valorDigitadoDois));
+                Console.WriteLine("Multiplicação: " + (valorDigitadoUm * valorDigitadoDois));
+                Console.WriteLine("Resto da divisão: " + (valorDigitadoUm % valorDigitadoDois));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
